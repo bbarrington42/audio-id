ec738ff0bc4e8839d88dc61fe985c9515f1bdc27
Bill Barrington <bill.barrington@candidpartners.com>
2019-08-17 13:29:09 -0400

    Yay. Works. Turned out to be permissions issue with media file in S3.

--- handler.js
+++ handler.js
@@ -9,16 +9,21 @@ const S = create ({
     env: env.concat (flutureEnv)
 });
 
-const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
+const AWS = require ('aws-sdk');
+const s3 = new AWS.S3 ();
 
 const Alexa = require ('alexa-sdk');
-const APP_ID = 'FILL ME IN!';
+const APP_ID = 'amzn1.ask.skill.03f0f63f-d84e-49a9-8fbd-cb069b642fa6';
+
+
+// Takes a Future of array, each element being a response from a listObjectsV2 request and concatenates
+// the elements in each 'Contents' array
+const getBucketContents = S.map (S.reduce (acc => elem => S.concat (acc) (elem.Contents)) ([]));
 
 // -----------------------------------------------------
 
 const states = {
-    STARTMODE: '_STARTMODE',
+    STARTMODE: '_STARTMODE'
 };
 
 /* INTENT HANDLERS */
@@ -26,9 +31,41 @@ const states = {
 const newSessionHandler = {
     'NewSession': function () {
         this.handler.state = states.STARTMODE;
-        this.emit (':ask', 'Welcome to Identify That Audio Clip');
+        this.emit (':ask', 'Welcome to Identify That Audio Clip. Would you like to play?');
     }
 };
+
+const startModeHandlers =
+    Alexa.CreateStateHandler (states.STARTMODE, {
+        'AMAZON.YesIntent': function () {
+            console.log (`YesIntent: ${JSON.stringify (this.event)}`);
+
+            // todo For now, just play the audio file and quit.
+
+            const audioFile = '<audio src="https://bills-audio-clips.s3.amazonaws.com/clips/blunders.mp3" />';
+            try {
+                this.emit (':tell', `${audioFile}`);
+            } catch (error) {
+                console.log (error);
+            }
+        },
+
+        'AMAZON.NoIntent': function () {
+            console.log (`NoIntent: ${JSON.stringify (this.event)}`);
+            this.emit (':tell', 'Ok, see you next time!');
+        },
+
+        'SessionEndedRequest': function () {
+            console.log ('Session ended');
+            this.emit (':tell', 'Your session has ended');
+        },
+
+        'Unhandled': function () {
+            console.error (`Unhandled: ${this.handler.state}`);
+            const message = 'Sorry, I don\'t know what to do now';
+            this.emit (':ask', message, 'Please try again');
+        }
+    });
 
 exports.handler = function (event, context) {
     console.log (`REQUEST++++${JSON.stringify (event)}`);
@@ -37,7 +74,7 @@ exports.handler = function (event, context) {
 
     alexa.appId = APP_ID;
 
-    alexa.registerHandlers (newSessionHandler);
+    alexa.registerHandlers (newSessionHandler, startModeHandlers);
 
     alexa.execute ();
 
